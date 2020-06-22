00db4e601a90418e16658a5c8d28d2ed98324f7b
Nitish Singla <nitish.singla@hmcts.net>
2020-06-22 09:44:06 +0100

    Initial checkin for the batch service.

---
+++ src/main/java/uk/gov/hmcts/reform/judicialbookingbatch/JudicialBookingBatchApplication.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.judicialbookingbatch;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@Slf4j
+@SuppressWarnings("HideUtilityClassConstructor")
+public class JudicialBookingBatchApplication {
+
+    public static void main(String[] args) throws Exception {
+        ApplicationContext context = SpringApplication.run(JudicialBookingBatchApplication.class, args);
+        //Sleep added to allow app-insights to flush the logs
+        Thread.sleep(6000);
+        int exitCode = SpringApplication.exit(context);
+        log.info("JudicialBookingBatchApplication Application exiting with exit code " + exitCode);
+        System.exit(exitCode);
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/judicialbookingbatch/JudicialBookingBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.hmcts.reform.judicialbookingbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JudicialBookingBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
